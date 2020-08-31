09fa61d3bb52f924aa66ea589afb4078932329fc
Samantha Andrews <samantharaeandrews@gmail.com>
2020-08-31 09:25:35 -0600

    cleanup

--- scripts.js
+++ scripts.js
@@ -14,49 +14,48 @@ function toggleSection(sectionId) {
   }
 }
 
-function copyURL(){
-  const url = window.location.href
-  const input =document.createElement('input');
+function copyURL() {
+  const url = window.location.href;
+  const input = document.createElement("input");
   document.body.appendChild(input);
   input.value = url;
   input.select();
-  document.execCommand('copy', false);
+  document.execCommand("copy", false);
   input.remove();
   // change button text, wait 5 seconds and then change back
   const button = document.getElementById("copy-link-button");
-  button.textContent = "Copied!"
-  setTimeout(function() {button.textContent = "Share link"}, 4000);
+  button.textContent = "Copied!";
+  setTimeout(function () {
+    button.textContent = "Share link";
+  }, 4000);
 }
-
-//
-function fbShare() {
-
-}
-
 
 // Twitter SDK
-window.twttr = (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return t;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    
-      t._e = [];
-      t.ready = function(f) {
-        t._e.push(f);
-      };
-    
-      return t;
-    }(document, "script", "twitter-wjs"));
+window.twttr = (function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+})(document, "script", "twitter-wjs");
 
 // Facebook SDK
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
